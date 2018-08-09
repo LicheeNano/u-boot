8a47beabdcf5a2a5ee03d3b7d047081445b60882
tongjinlv <179990830@qq.com>
2018-08-09 10:38:49 +0800

    ./git.sh

---
+++ make.sh
@@ -0,0 +1,4 @@
+PATH="$PATH:/opt/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabi/bin"
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- licheepi_nano_spiflash_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
+
